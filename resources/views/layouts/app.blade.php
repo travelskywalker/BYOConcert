7195025e4bfed80508d492a3da2a3807f99257c9
lei_mendenilla <https://lei_mendenilla@bitbucket.org>
2018-07-10 14:21:07 +0800

    add meta OG to the website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="BITE AND SLURP YOUR WAY TO A FREE CONCERT">
+    <meta property="og:site_name" content="711 Build Your Own Concert">
+    <meta property="og:image" content="/images/og_prev.jpg">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
