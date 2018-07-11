5f553a6dee71a2f4376a35a249c0365297344d66
lei_mendenilla <https://lei_mendenilla@bitbucket.org>
2018-07-11 08:52:07 +0800

    clean app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,22 +14,11 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-   
-
-    <!-- Fonts -->
-    <!-- <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css"> -->
-
     <!-- Styles -->
-    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     <link href="/materialize/css/materialize.min.css" rel="stylesheet">
     <link href="/css/main.css" rel="stylesheet">
     <link href="/css/fonts.css" rel="stylesheet">
 
-
-
-     
-   
 </head>
 <body>
 <!-- Modal Structure -->
