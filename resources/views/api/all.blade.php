8f157e27b08a8544450fd7a4849023892d1f3c27
lei_mendenilla <https://lei_mendenilla@bitbucket.org>
2018-07-12 11:08:33 +0800

    add rank count in table api

--- resources/views/api/all.blade.php
+++ resources/views/api/all.blade.php
@@ -3,14 +3,16 @@
     <td style="border:1px solid black">School</td>
     <td style="border:1px solid black">Nomination</td>
 </tr>
+<?php $c = 1 ?>
 @foreach($data as $result)
 <tr>
     <td style="border:1px solid black">
-        {{$result->school_name}}
+        {{$c.'. '.$result->school_name}}
     </td>
     <td style="border:1px solid black; text-align: center">
         {{$result->vote_count}}
     </td>
 </tr>
+<?php $c++;?>
 @endforeach
 </table>
